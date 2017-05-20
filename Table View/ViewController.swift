ba8d4faa654c87965f5a677b6966afe6875fc582
RASHED <hello@MacBooks-MacBook-Pro.local>
2017-05-20 21:33:00 +0600

    table view

--- Table View/ViewController.swift
+++ Table View/ViewController.swift
@@ -8,7 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+   
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+    
+     return 1
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+    
+    let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = "Top row"
+        
+        return cell
+    
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
