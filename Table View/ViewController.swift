9051ca5e4b3e27504b62e86dc00228181097f3e4
MacBook Retina <hello@macbooks-macbook-pro.local>
2017-05-20 21:21:25 +0600

    Initial Commit

---
+++ Table View/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Table View
+//
+//  Created by RASHED on 5/20/17.
+//  Copyright © 2017 RASHED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
